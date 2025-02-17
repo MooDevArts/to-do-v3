ab6b22f5540332166280e7df5b5592ac899ea8e1
Himanshu <himanshu.a.04.1999@gmail.com>
2025-02-17 08:29:28 -0500

    Add Menu and home page skeletons

---
+++ components/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <button className="green">Add</button>
+    </div>
+  );
+};
+
+export default Menu;
